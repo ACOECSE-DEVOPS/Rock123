3b7e87f06e6c82ed8fda59d40135ab9a11e1df7c
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-11-11 11:42:50 +0530

    first

---
+++ src/main/java/DPJenkinsmaven/Rock123/App.java
@@ -0,0 +1,13 @@
+package DPJenkinsmaven.Rock123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to my Jenkins world" );
+    }
+}
